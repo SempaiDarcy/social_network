064963e29cc16d707347cff691e1047d0e07cd40
mr_darcy <mr_darcy_1@mail.ru>
2022-11-23 23:46:17 +0500

    Commit

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import s from './Header.module.css'
  const Header = () => {
     return <header className={s.header}>
+
                 <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ-5dGGUlrhbGe1yvrabfUwokLJ_y47ffYgA&usqp=CAU'/>
+
             </header>
 
 };
