d8460e6dbbcb5a93cabcd83b512db0df102c7a3e
SempaiDarcy <mr_darcy_1@mail.ru>
2023-06-17 21:27:45 +0500

    configuring the useref hook in textarea

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,3 +1,4 @@
+import React, { useRef } from "react";
 import { PostType } from "../../App";
 import { Post } from "../Post/Post";
 import s from "./Profile.module.css";
@@ -6,18 +7,29 @@ type ProfilePeropsType = {
   posts: PostType[];
 };
 export const Profile = (props: ProfilePeropsType) => {
+  const postsElements = props.posts.map((el) => {
+    return <Post message={el.message} likes={el.likes} />;
+  });
+
+  const newPostElement = useRef<HTMLTextAreaElement>(null);
+  const addPost = () => {
+    let text = newPostElement.current?.value;
+    alert(text);
+  };
   return (
     <div className={s.content}>
       <div>
         My posts
         <div>
-          <textarea></textarea>
-          <button>Add Post</button>
+          <div>
+            <textarea ref={newPostElement}></textarea>
+          </div>
+          <div>
+            <button onClick={addPost}>Add Post</button>
+          </div>
         </div>
         <div className={s.posts}>
-          {props.posts.map((el) => {
-            return <Post message={el.message} likes={el.likes} />;
-          })}
+          {postsElements}
           {/* <Post message="Hi" likes="1" />
           <Post message="How are u" likes="2" />
           <Post message="I am fine" likes="3" />
